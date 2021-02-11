e16ee2781c4c9e74283e25fdd12a480960fe009a
Nadiar Syaripul <codxse@gmail.com>
2021-02-11 13:33:23 +0700

    parser

---
+++ formula-parser/src/app.ts
@@ -0,0 +1,112 @@
+// @ts-ignore
+import {VALUES} from "./mock";
+
+const FormulaParser = require('hot-formula-parser').Parser;
+
+export type ValidationStatus = "ok" | "error"
+export interface ValidationResponse {
+  valid: boolean
+  status: ValidationStatus
+  message: string
+}
+
+export interface IFormulaParser {}
+
+export class LambdaParser implements IFormulaParser {
+  private parser = new FormulaParser()
+  precision: number = 2
+  lambda: string
+  scope: Record<string, any>
+
+  constructor(lambda: string, scope: Record<string, any>) {
+    this.lambda = lambda
+    this.scope = scope
+    this.initVars()
+  }
+
+  evaluate(): number {
+    const res = this.parser.parse(this.lambda)
+    if (res.error) {
+      throw new Error(res.error)
+    }
+    return LambdaParser.parseNumber(res.result, this.precision)
+  }
+
+  private initVars() {
+    for (const [key, value] of Object.entries(this.scope)) {
+      const variables = key.split(/\./)
+      const variable = variables[variables.length - 1]
+      this.parser.setVariable(variable, value)
+    }
+  }
+
+  private cleanse(): string {
+    return ""
+  }
+
+  /**
+   * validate string formula
+   * @param {string} lambda
+   * @param {Record<string, any>} scope
+   * @return {ValidationResponse}
+   */
+  static validateFormula(lambda: string, scope: Record<string, any> = {}): ValidationResponse {
+    // 1. check if formula valid, if valid continue
+    const parser = new LambdaParser(lambda, scope)
+    try {
+      parser.evaluate()
+      return {
+        valid: true,
+        status: "ok",
+        message: `OK`
+      }
+    } catch (e) {
+      // 2. Check if every variable is exist, if exist pass
+      const variables = lambda.split(/[\+\s\-\*\\()]+|AVERAGE|SUM/g).filter(v => (v[0] || "").match(/^[a-zA-Z_]+/g))
+      if (variables.length > 0) {
+        const validationStatus = variables.map((v) => {
+          // if array var
+          if (v.split("/\./g").length > 1) {
+            const [arrayName, variableName] = v.split("/\./g")
+          }
+          const valid = Boolean(scope[v])
+          return {
+            valid,
+            status: valid ? "ok" : "error",
+            message: valid ? "" : `Variable ${v} doesn't exist`
+          }
+        })
+
+        // 2. Check if every variable is exist, if exist pass
+        const response = validationStatus.reduce((acc, current) => {
+          const nextValid = acc.valid && current.valid
+          return {
+            valid: nextValid,
+            status: nextValid ? "ok" : "error",
+            message: acc.message + (current.message !== "" ? `, ${current.message}` : "")
+          }
+        })
+
+        return response as ValidationResponse
+      }
+
+      return {
+        valid: false,
+        status: "error",
+        message: `Invalid formula! ${e.message}`
+      }
+    }
+  }
+
+  /**
+   * parse any string number to float, if it giving NaN it will return 0.0
+   * @param {string} number
+   * @param {number} precision
+   * @return {number}
+   */
+  static parseNumber(number: string | number, precision: number = 2): number {
+    const nextNumber: string = `${number}`
+    const prec = parseFloat(nextNumber).toFixed(precision)
+    return parseFloat(prec)
+  }
+}
---
+++ formula-parser/src/mock.ts
@@ -0,0 +1,44 @@
+export const VALUES = {
+  "unlisted.name_qz32951218c74d": "nadiar",
+  "unlisted.alamat_qzdc583d2c55b4": "garut",
+  "unlisted.tanggal_lahir_qz0c6f92a3b436": "2018-02-10T05:34:00.000Z",
+  "unlisted.Kota_qz3457e8f7740d": "Garut",
+  "unlisted.Faktor_qz5eb5371eb297": "0.3",
+  "unlisted.constant_qz2ac8b5e99415": 12,
+  "string_one": "1",
+  "integer_one": 1,
+  "unlisted.Uang_saku_qz0c66d1c9b872": 12500000,
+  "Array_Section_qzdd3b55058309.Keluarga_qz9fe7fdbb683d": [
+    {
+      "Nama_ayah_qz7dcc9ea59565": "Abi",
+      "Alamat_qzc0352ec42d97": "Garut",
+      "Jumlah_anak_qzb42b12e0cd2f": "1",
+      "Pendapatan_keluarga_qz13b2b7515449": "50.000.000"
+    },
+    {
+      "Nama_ayah_qz7dcc9ea59565": "Abu",
+      "Alamat_qzc0352ec42d97": "Arab",
+      "Jumlah_anak_qzb42b12e0cd2f": "3",
+      "Pendapatan_keluarga_qz13b2b7515449": "100.000.000"
+    }
+  ],
+  "Pendapatan_Ibu_qz1a05b08fa4f9.Data_Ibu_qzae1f0194e67a": [
+    {
+      "Nama_Ibu_qz043ad0f702a1": "Popi",
+      "Pendapatan_qz8cbbe14c630f": "12.500.000",
+      "Jumlah_Anak_qz2a1a736fa975": "1"
+    },
+    {
+      "Nama_Ibu_qz043ad0f702a1": "Popo",
+      "Pendapatan_qz8cbbe14c630f": "1.000.000",
+      "Jumlah_Anak_qz2a1a736fa975": "3"
+    },
+    {
+      "Nama_Ibu_qz043ad0f702a1": "Pupu",
+      "Pendapatan_qz8cbbe14c630f": "300.000",
+      "Jumlah_Anak_qz2a1a736fa975": "0"
+    }
+  ],
+  "document.name": "MOCK Forumla",
+  "document.title": "Formula MOCK Test"
+}
