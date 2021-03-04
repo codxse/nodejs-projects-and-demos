b2c519e20944cca9a874fe40c32acff8d077e314
Nadiar Syaripul <codxse@gmail.com>
2021-03-05 05:29:40 +0700

    more test

--- template-rgx-test/tests/array_table.test.ts
+++ template-rgx-test/tests/array_table.test.ts
@@ -9,6 +9,13 @@ describe("Regex array table test", () => {
     expect(template.toArrayIndex(["li", "tr"]).mustache).to.equal(expected)
   })
 
+  it("index array 2", () => {
+    const raw = `<mark class="template-variable2 Unlisted number_qz51e317cdbfcf name1_qzed2f1cb5edc2" title="number" contenteditable="false" data-variable="number_qz51e317cdbfcf" data-vartype="arrayIndex" data-section="Unlisted" data-arrayname="name1_qzed2f1cb5edc2">&lt;&lt;Unlisted.name1.number&gt;&gt;</mark>`
+    const expected = `{{@index + 1}}`
+    const template = new Template(raw)
+    expect(template.toArrayIndex(["li", "tr"]).mustache).to.equal(expected)
+  })
+
   it("table1", () => {
     const raw = `<table><tr class="array" data-section="unlisted" data-varname="identitas_qz946df61286d8" data-vartype="array"><td><p><strong><mark class="template-variable2 unlisted name_qz1bb8edeaff06" contenteditable="false" data-variable="name_qz1bb8edeaff06" data-vartype="text" data-section="unlisted" data-arrayname="identitas_qz946df61286d8">&lt;&lt;unlisted.identitas.name&gt;&gt;</mark></strong></p></td><td><p><strong>Description Of Work </strong></p></td><td><p><strong>Technical</strong></p></td></tr><tr><td><p>&lt;&lt;variables&gt;&gt;</p></td><td></td><td></td></tr><tr><td></td><td><p>&lt;&lt;variables&gt;&gt;</p></td><td></td></tr><tr><td></td><td></td><td><p>&lt;&lt;variables&gt;&gt;</p></td></tr></table>`
     const expected = "<table>{{#unlisted.identitas_qz946df61286d8}}<tr class=\"array\" data-section=\"unlisted\" data-varname=\"identitas_qz946df61286d8\" data-vartype=\"array\"><td><p><strong>{{name_qz1bb8edeaff06}}</strong></p></td><td><p><strong>Description Of Work </strong></p></td><td><p><strong>Technical</strong></p></td></tr>{{/unlisted.identitas_qz946df61286d8}}<tr><td><p>&lt;&lt;variables&gt;&gt;</p></td><td></td><td></td></tr><tr><td></td><td><p>&lt;&lt;variables&gt;&gt;</p></td><td></td></tr><tr><td></td><td></td><td><p>&lt;&lt;variables&gt;&gt;</p></td></tr></table>"
